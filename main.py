2d884095f22cee898655f26c34bd06f851e5bd0f
EvanSchoolGit <158589390+EvanSchoolGit@users.noreply.github.com>
2024-06-10 01:45:33 +0000

    Dialouge Updates

--- main.py
+++ main.py
@@ -38,15 +38,21 @@ corpse_discovery = 0
 vixen_roomentry = 0
 kidlock = 0
 safeprogress = 0
+vixenstress = 0
+oliverstress = 0
+lucasstress = 0
 "Child Class Setups"
 
 
 """File Setups"""
 mapfile = 'map.txt'
 person = 'person.txt'
+personstress = 'personstress.txt'
 movementui = 'movementui.txt'
 mainui = 'mainui.txt'
 autopsyfile = 'autopsy.txt'
+gameoverfile = 'gameover.txt'
+gameoverwilson = 'gameoverwilson.txt'
 
 rooms = {
   "Room 118": {
@@ -76,6 +82,18 @@ def cls():
 
 def intro():
   input("")
+
+def oliverend():
+  pass
+
+def lucasend():
+  pass
+
+def vixenend():
+  pass
+
+def wilsonend():
+  pass
 
 def tutorial():
   #DRAFT TEXT, FEEL FREE TO CHANGE ANYTHING, THIS IS ALL RUBBISH
@@ -280,19 +298,167 @@ def roomdesc():
     print ("[PRESS Q] ... What do I need to again?")
   
 def suspectroom():
-  #INTEROGATION AND DIALOGUE SYSTEM
-  pass
+  input("*You step inside...")
+  input("*What should I do first")
+  while True:
+    cls()
+    print("")
+    print("------------------------------------")
+    print("[1] TALK TO OLIVER")
+    print("[2] TALK TO LUCAS")
+    print("[3] TALK TO VIXEN")
+    print("[4] DO SOMETHING ELSE")
+    print("[5] MAKE AN ARREST")
+    print("------------------------------------")
+    print("")
+    suspectroomchoice = input("CHOICE: ")
+    if suspectroomchoice == "1":
+      olivertalk()
+      pass
+    if suspectroomchoice == "2":
+      lucastalk()
+      pass
+    if suspectroomchoice == "3":
+      vixentalk()
+      pass
+    if suspectroomchoice == "4":
+      break
+    if suspectroomchoice == "5":
+      pass
+    
+
   
 def vixentalk():
+  global vixenstress
+  while True:
+    cls()
+    if vixenstress == 0:
+      with open(person) as file:
+        print(file.read())
+    elif vixenstress == 2:
+      with open(personstress) as file:
+        print(file.read())
+    print("--------------------------------------------------")
+    print("[1] How did you know the victim? ")
+    print("[2] What did you know about the murder? ")
+    if rent_bill.progress == 1:
+      print("[3] Why are you still living here without paying all of your rent?")
+    if rent_bill.progress == 1 and vixen_gun.progress == 1:
+      print("[4] Did you being poor and desperate ever factor into the murder?")
+    print("[5] I'm done with you")
+    print("--------------------------------------------------")
+    vixentalkquest = input("CHOICE: ")
+    cls()
+    if vixentalkquest == "1":
+      print("> How did you know the victim?")
+      print("")
+      input("'She was my neighbor'")
+      print("")
+      input("'...'")
+      print("[1] Just a neighbor?")
+      print("[2] Anything else?")
+      pass
+    if vixentalkquest == "2":
+      pass
+    if vixentalkquest == "3":
+      pass
+    if vixentalkquest == "4":
+      pass
+    if vixentalkquest == "5":
+      break
+
+
+    
   #VIXEN DIALOGUE AND QUESTIONING
   pass
   
 def lucastalk():
+  global lucasstress
+  while True:
+    cls()
+    if lucasstress == 0:
+      with open(person) as file:
+        print(file.read())
+    elif lucasstress == 2:
+      with open(personstress) as file:
+        print(file.read())
+    print("--------------------------------------------------")
+    print("[1] How did you know the victim? ")
+    print("[2] What did you know about the murder? ")
+    if lucas_gun.progress == 1:
+      print("[3] Tell me why you have a gun in your room?")
+    print("[4] I'm done with you")
+    print("--------------------------------------------------")
+    lucastalkquest = input("CHOICE: ")
+    cls()
+    if lucastalkquest == "1":
+      pass
+    if lucastalkquest == "2":
+      pass
+    if lucastalkquest == "3":
+      pass
+    if lucastalkquest == "4":
+      break
+
   #LUCAS DIALOGUE AND QUESTIONING
   pass
   
 def olivertalk():
+  global oliverstress
+  while True:
+    cls()
+    if oliverstress == 0:
+      with open(person) as file:
+        print(file.read())
+    elif lucasstress == 2:
+      with open(personstress) as file:
+        print(file.read())
+    print("--------------------------------------------------")
+    print("[1] How did you know the victim? ")
+    print("[2] What did you know about the murder? ")
+    if safe_keycard.progress == 1:
+      print("[3] Where did you find this?")
+    print("[4] I'm done with you")
+    print("--------------------------------------------------")
+    olivertalkquest = input("CHOICE: ")
+    cls()
+    if olivertalkquest == "1":
+      pass
+    if olivertalkquest == "2":
+      pass
+    if olivertalkquest == "3":
+      pass
+    if olivertalkquest == "4":
+      break
+
   #OLIVER DIALOGUE AND QUESTIONING
+  pass
+
+def arrest():
+  cls()
+  print("(WARNING, THIS IS THE FINAL DESCISION, BE CAUTIOUS)")
+  print("")
+  print("--------------------------------------------------")
+  print("[1] ARREST OLIVER")
+  print("[2] ARREST LUCAS")
+  print("[3] ARREST VIXEN")
+  print("[4] ARREST WILSON")
+  print("[5] Let me rethink this...")
+  print("--------------------------------------------------")
+  while True:
+    arrestchoice = input("CHOICE")
+    if arrestchoice == "1":
+      pass
+    elif arrestchoice == "2":
+      pass
+    elif arrestchoice == "3":
+      pass
+    elif arrestchoice == "4":
+      pass
+    elif arrestchoice == "5":
+      break
+    else:
+      pass
   pass
   
 def vixenroomentry():
